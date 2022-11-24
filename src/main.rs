555cd454a776c257c280c83ae37cca6126d36a44
Neptunal <110062414+nptnl@users.noreply.github.com>
2022-11-24 14:43:42 -0500

    basic three-word evalutaion
    we're well on the way, now for the hard part...

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Token {
     Val(i32),
     Op(Oprtr),
     Begin(i32),
     End(i32),
 }
-#[derive(Debug)]
+impl Token {
+    fn extract(self) -> Result<i32, ()> {
+        match self {
+            Self::Val(v) => Ok(v),
+            _ => Err(println!("you real ugly bruv")),
+        }
+    }
+}
+#[derive(Debug, PartialEq)]
 enum Oprtr {
     Add,
     Sub,
@@ -20,35 +28,36 @@ fn main() {
     .replace('(', " ( ")
     .replace(')', " ) ");
     let input: Vec<&str> = input.split_whitespace().collect();
-    println!("{:#?}", tokenize(input))
+    println!("{:?}", basic((Token::Op(Oprtr::Add), Token::Val(3), Token::Val(4))))
 }
-
-fn tokenize(input: Vec<&str>) {
+fn tokenize(input: Vec<&str>) -> Vec<Token> {
     let mut tkvec: Vec<Token> = Vec::new();
     let mut depth: i32 = 0;
     for word in input {
         let to_add: Result<Token, ()> = match word {
-            "(" => Ok(Token::Begin(depth + 1)),
-            ")" => Ok(Token::End(depth - 1)),
+            "(" => { depth += 1; Ok(Token::Begin(depth)) },
+            ")" => { depth -= 1; Ok(Token::End(depth + 1)) },
             "+" => Ok(Token::Op(Oprtr::Add)),
             "-" => Ok(Token::Op(Oprtr::Sub)),
             "*" => Ok(Token::Op(Oprtr::Mul)),
             "//" => Ok(Token::Op(Oprtr::Div)),
-            _ => match word.parse() {
+            _ => match word.parse::<i32>() {
                 Ok(v) => Ok(Token::Val(v)),
                 Err(_) => Err(println!("get good, invalid token")),
             }
         };
         tkvec.push(to_add.unwrap());
     }
+    tkvec
 }
-
-/*
-i just had an idea i cant forget
-so right now im using a Vec<&str> and then tokenizing each word of it
-and then later using depth and recursion to do parenthesis
-but i can do this while tokenizing, i can calculate the depth
-and make the depth an argument to the token enum
-like Token::Begin(i32) and Token::End(i32) taking depth into account
-
-*/
+fn basic(set: (Token, Token, Token)) -> Result<i32, ()> {
+    let v1 = set.1.extract().unwrap();
+    let v2 = set.2.extract().unwrap();
+    match set.0 {
+        Token::Op(Oprtr::Add) => Ok(v1 + v2),
+        Token::Op(Oprtr::Sub) => Ok(v1 - v2),
+        Token::Op(Oprtr::Mul) => Ok(v1 * v2),
+        Token::Op(Oprtr::Div) => Ok(v1 / v2),
+        _ => Err(println!("you suck")),
+    }
+}

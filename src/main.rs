9e2506cfbe35219a69518345250446863b53e36c
Neptunal <110062414+nptnl@users.noreply.github.com>
2022-11-26 14:19:16 -0500

    generalization
    syntax is now changed ti style `+ {val} {val} ....`, meaning there can be any amount of values to a math function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum Token {
     Val(i32),
-    Op(Oprtr),
+    Op(Mfn),
     Begin(i32),
     End(i32),
 }
@@ -14,23 +14,22 @@ impl Token {
     }
 }
 #[derive(Debug, PartialEq, Clone, Copy)]
-enum Oprtr {
+enum Mfn {
     Add,
     Sub,
     Mul,
     Div,
+    Square,
 }
 
 fn main() {
     let mut input: String = String::new();
-    loop {
-        std::io::stdin().read_line(&mut input).expect("failed to read");
-        let input = input
-        .replace('(', " ( ")
-        .replace(')', " ) ");
-        let input: Vec<&str> = input.split_whitespace().collect();
-        println!("{:?}", complete(tokenize(input)).unwrap())
-    }
+    std::io::stdin().read_line(&mut input).expect("failed to read");
+    let input = input
+    .replace('(', " ( ")
+    .replace(')', " ) ");
+    let input: Vec<&str> = input.split_whitespace().collect();
+    println!("{:?}", complete(tokenize(input)).unwrap())
 }
 fn tokenize(input: Vec<&str>) -> Vec<Token> {
     let mut tkvec: Vec<Token> = Vec::new();
@@ -39,10 +38,11 @@ fn tokenize(input: Vec<&str>) -> Vec<Token> {
         let to_add: Result<Token, ()> = match word {
             "(" => { depth += 1; Ok(Token::Begin(depth)) },
             ")" => { depth -= 1; Ok(Token::End(depth + 1)) },
-            "+" => Ok(Token::Op(Oprtr::Add)),
-            "-" => Ok(Token::Op(Oprtr::Sub)),
-            "*" => Ok(Token::Op(Oprtr::Mul)),
-            "//" => Ok(Token::Op(Oprtr::Div)),
+            "+" | "add" => Ok(Token::Op(Mfn::Add)),
+            "-" | "sub" => Ok(Token::Op(Mfn::Sub)),
+            "*" | "mul" => Ok(Token::Op(Mfn::Mul)),
+            "//" | "div" => Ok(Token::Op(Mfn::Div)),
+            "sq" => Ok(Token::Op(Mfn::Square)),
             _ => match word.parse::<i32>() {
                 Ok(v) => Ok(Token::Val(v)),
                 Err(_) => Err(println!("get good, invalid token")),
@@ -52,24 +52,55 @@ fn tokenize(input: Vec<&str>) -> Vec<Token> {
     }
     tkvec
 }
-fn basic(operation: Token, v1: Token, v2: Token) -> Result<i32, ()> {
-    let v1 = v1.extract().unwrap();
-    let v2 = v2.extract().unwrap();
-    match operation {
-        Token::Op(Oprtr::Add) => Ok(v1 + v2),
-        Token::Op(Oprtr::Sub) => Ok(v1 - v2),
-        Token::Op(Oprtr::Mul) => Ok(v1 * v2),
-        Token::Op(Oprtr::Div) => Ok(v1 / v2),
-        _ => Err(println!("invalid operator error")),
-    }
+fn oneop(expr: Vec<Token>) -> Result<i32, ()> {
+    let mut output: i32 = 0;
+    match expr[0] {
+        Token::Op(Mfn::Add) => for addend in expr[1..].to_vec() {
+            output += addend.extract().unwrap();
+        },
+        Token::Op(Mfn::Sub) => {
+            output = expr[1].extract().unwrap();
+            for addend in expr[2..].to_vec() {
+                output -= addend.extract().unwrap();
+            };
+        },
+        Token::Op(Mfn::Mul) => {
+            output = 1i32;
+            for factor in expr[1..].to_vec() {
+            output *= factor.extract().unwrap();
+            };
+        },
+        Token::Op(Mfn::Div) => {
+            output = expr[1].extract().unwrap();
+            for factor in expr[2..].to_vec() {
+                output /= factor.extract().unwrap();
+            };
+        },
+        Token::Op(Mfn::Square) => {
+            output = 1i32;
+            for factor in expr[1..].to_vec() {
+                output *= factor.extract().unwrap() * factor.extract().unwrap()
+            };
+        },
+        _ => return Err(println!("invalid operator, returning zero in this operation")),
+    };
+    Ok(output)
 }
 fn complete(mut keys: Vec<Token>) -> Result<i32, ()> {
-    if keys.len() == 1 {
-        keys[0].extract()
-    } else if keys.len() == 3 {
-        basic(keys[1], keys[0], keys[2])
-    } else if keys.len() == 5 {
-        basic(keys[2], keys[1], keys[3])
+    let le: usize = keys.len();
+    match (keys[0], keys[le-1]) {
+        (Token::Begin(d1), Token::End(d2)) => if d1 == d2 { return complete(keys[1..le-1].to_vec()) }
+        _ => (),
+    }
+    let mut le: bool = true;
+    for word in &keys {
+        match word {
+            Token::Begin(_d) => { le = false; break },
+            _ => continue,
+        }
+    }
+    if le {
+        return oneop(keys)
     } else {
         let mut simpler: Vec<Token> = Vec::new();
         let mut indx: usize = 0;
@@ -94,9 +125,7 @@ fn find_end(keys: &Vec<Token>, begin: usize, depth: i32) -> usize {
     let mut indx = begin;
     loop {
         indx += 1;
-        if keys[indx] == Token::End(depth) {
-            break
-        }
+        if indx == keys.len() { return begin }
+        else if keys[indx] == Token::End(depth) { return indx }
     }
-    indx
 }

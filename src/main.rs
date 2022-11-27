c7dac6b2ea03bfe5cdb8cf5127d7de7b8c43d857
Neptunal <110062414+nptnl@users.noreply.github.com>
2022-11-26 21:02:33 -0500

    comp support
    now, instead of f32, we use full Ferrum complex numbers.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use ferrum::ch::Comp;
+
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum Token {
-    Val(f32),
+    Val(Comp),
     Op(Mfn),
     Begin(i32),
     End(i32),
 }
 impl Token {
-    fn extract(self) -> Result<f32, ()> {
+    fn extract(self) -> Result<Comp, ()> {
         match self {
             Self::Val(v) => Ok(v),
             _ => Err(println!("you real ugly bruv")),
@@ -43,7 +45,7 @@ fn tokenize(input: Vec<&str>) -> Vec<Token> {
             "*" | "mul" => Ok(Token::Op(Mfn::Mul)),
             "/" | "div" => Ok(Token::Op(Mfn::Div)),
             "sq" => Ok(Token::Op(Mfn::Square)),
-            _ => match word.parse::<f32>() {
+            _ => match word.parse::<Comp>() {
                 Ok(v) => Ok(Token::Val(v)),
                 Err(_) => Err(println!("get good, invalid token")),
             }
@@ -52,9 +54,10 @@ fn tokenize(input: Vec<&str>) -> Vec<Token> {
     }
     tkvec
 }
-fn oneop(expr: Vec<Token>) -> Result<f32, ()> {
-    let mut output: f32 = 0.0;
+fn oneop(expr: Vec<Token>) -> Result<Comp, ()> {
+    let mut output: Comp = ferrum::ch::CC0;
     match expr[0] {
+        Token::Val(v) => output = v,
         Token::Op(Mfn::Add) => for addend in expr[1..].to_vec() {
             output += addend.extract().unwrap();
         },
@@ -65,7 +68,7 @@ fn oneop(expr: Vec<Token>) -> Result<f32, ()> {
             };
         },
         Token::Op(Mfn::Mul) => {
-            output = 1f32;
+            output = ferrum::ch::CC1;
             for factor in expr[1..].to_vec() {
             output *= factor.extract().unwrap();
             };
@@ -77,7 +80,7 @@ fn oneop(expr: Vec<Token>) -> Result<f32, ()> {
             };
         },
         Token::Op(Mfn::Square) => {
-            output = 1f32;
+            output = ferrum::ch::CC1;
             for factor in expr[1..].to_vec() {
                 output *= factor.extract().unwrap() * factor.extract().unwrap()
             };
@@ -86,7 +89,7 @@ fn oneop(expr: Vec<Token>) -> Result<f32, ()> {
     };
     Ok(output)
 }
-fn complete(mut keys: Vec<Token>) -> Result<f32, ()> {
+fn complete(mut keys: Vec<Token>) -> Result<Comp, ()> {
     let le: usize = keys.len();
     match (keys[0], keys[le-1]) {
         (Token::Begin(d1), Token::End(d2)) => if d1 == d2 { return complete(keys[1..le-1].to_vec()) }
